4e7c42652346ec380ea104c4473aa2049b950fcd
Roman Nix <rnix@yandex.ru>
2025-10-06 19:17:25 +0000

    Refactor front end

---
+++ public/js/game_event_handler.js
@@ -0,0 +1,140 @@
+const GameEventHandler = {
+    FireballEvent(data) {
+        this.bullets.fireBullet(data.clientId, data.x, data.y, data.direction)
+    },
+
+    CreaturesStatsUpdateEvent(data) {
+        for (const p of data.players) {
+            const id = p.clientId;
+            if (id === this.myClientId) {
+                if (this.player.hp !== p.hp) {
+                    this.player.hp = p.hp;
+                    if (this.player.hpText) {
+                        this.player.hpText.setText(p.hp + '/100');
+                    }
+                }
+                continue;
+            }
+            let justSpawned = false;
+            if (!this.players[id]) {
+                // spawn new player
+                const np = this.physics.add.sprite(p.x, p.y, 'player', 1).setScale(PLAYER_SCALE).setDepth(DEPTH_PLAYER);
+                np.id = id;
+                np.hp = p.hp;
+
+                np.setTint(Math.random() * 0xffffff);
+
+                np.hpText = this.add.text(p.x, p.y, p.hp + '/100', { font: '8px Arial', fill: '#ffffff' }).setOrigin(0.5, 1).setDepth(DEPTH_PLAYER + 1);
+
+                this.players[id] = np;
+                this.bullets.addPlayer(np);
+
+                justSpawned = true;
+                console.log('spawn player', id, Object.keys(this.players).length);
+            }
+
+            this.updatePlayerPos(p);
+
+            const pSprite = this.players[id];
+            if (pSprite.hp !== p.hp || justSpawned) {
+                pSprite.hp = p.hp;
+                if (pSprite.hpText) {
+                    pSprite.hpText.setText(p.hp + '/100');
+                }
+                if (p.hp === 0) {
+                    if (pSprite.hpText) {
+                        pSprite.hpText.destroy();
+                        pSprite.setTint(0xff3333);
+                        // avoid late changes of damage effect
+                        this.time.delayedCall(110, () => pSprite.setTint(0xff3333), [], this);
+                        pSprite.setDepth(DEPTH_DEAD);
+                        pSprite.disableBody();
+                    }
+                }
+            }
+        }
+
+        for (const m of data.monsters) {
+            const id = m.id;
+            if (!this.monsters[id]) {
+                // spawn new monster
+                this.monsters[id] = Monster.SpawnNewMonster(this, m);
+                this.bullets.addMonster(this.monsters[id]);
+            }
+
+            this.monsters[id].updateStatAndPosition(m);
+        }
+    },
+
+    CreaturesPosUpdateEvent(data) {
+        for (const p of data.players) {
+            this.updatePlayerPos(p)
+        }
+
+        for (const m of data.monsters) {
+            this.updateMonsterPos(m);
+        }
+    },
+
+    PlayerDeathEvent(data) {
+        if (data.clientId === this.myClientId) {
+            this.add.text(this.scale.width / 2, this.scale.height / 2, 'YOU DIED', { font: '24px Arial', fill: '#ff0000' })
+                .setOrigin(0.5, 0.5)
+                .setScrollFactor(0, 0)
+                .setDepth(DEPTH_UI);
+            this.isDead = true;
+            console.log('this is my death');
+        }
+    },
+
+    ArrowEvent(data) {
+        const ar = this.physics.add.sprite(data.x1, data.y1, 'arrow', 0).setScale(2);
+        const dir = new Phaser.Math.Vector2(data.x2 - data.x1, data.y2 - data.y1).normalize();
+        ar.setVelocity(dir.x * 400, dir.y * 400);
+        this.physics.add.collider(ar, this.layerWalls, () => ar.destroy(), null, this);
+        this.physics.add.overlap(ar, this.player, () => {
+            ar.destroy();
+            this.sendGameCommand('HitPlayerCommand', {
+                monsterId: data.monsterId,
+                targetClientId: this.myClientId
+            });
+        }, null, this);
+    },
+
+    DemonFireballEvent(data) {
+        const ar = this.physics.add.sprite(data.x1, data.y1, 'bullet', 0).setScale(1);
+        const dir = new Phaser.Math.Vector2(data.x2 - data.x1, data.y2 - data.y1).normalize();
+        ar.setVelocity(dir.x * 700, dir.y * 700);
+        this.physics.add.collider(ar, this.layerWalls, () => ar.destroy(), null, this);
+        this.physics.add.overlap(ar, this.player, () => {
+            ar.destroy();
+            this.sendGameCommand('HitPlayerCommand', {
+                monsterId: data.monsterId,
+                targetClientId: this.myClientId
+            });
+        }, null, this);
+    },
+
+    DamageEvent(data) {
+        const pId = data.targetPlayerId;
+        const mId = data.targetMonsterId;
+        if (pId === this.myClientId) {
+            this.player.setTint(0xff0000);
+            this.time.delayedCall(100, () => this.player.clearTint(), [], this);
+        }
+        for (const i in this.players) {
+            const p = this.players[i];
+            if (p.id === pId) {
+                p.setTint(0xff0000);
+                this.time.delayedCall(100, () => p.clearTint(), [], this);
+            }
+        }
+        for (const i in this.monsters) {
+            const m = this.monsters[i];
+            if (m.id === mId) {
+                m.setTint(0xff0000);
+                this.time.delayedCall(100, () => m.clearTint(), [], this);
+            }
+        }
+    },
+}
---
+++ public/js/monsters.js
@@ -0,0 +1,168 @@
+class Monster extends Phaser.Physics.Arcade.Sprite
+{
+    id;
+    kind;
+    maxHp = 100;
+    hp = 100;
+    hpText;
+    isAttacking = false;
+    scene;
+    isCorpse = false;
+
+    constructor (scene, statData, spriteKey, frame)
+    {
+        super(scene, statData.x, statData.y, spriteKey, frame);
+
+        this.scene = scene;
+        this.spawn(statData);
+    }
+
+    spawn(statData)
+    {
+        this.x = statData.x;
+        this.y = statData.y;
+        this.setScale(2);
+        this.setDepth(DEPTH_MONSTER);
+
+        this.id = statData.id;
+        this.hp = statData.hp;
+        this.hpText = this.scene.add.text(statData.x, statData.y, statData.hp + '/' + this.maxHp, { font: '14px Arial', fill: '#ffffff' })
+            .setOrigin(0.5, 1)
+            .setDepth(DEPTH_MONSTER + 1);
+
+        if (this.hp <= 0) {
+            this.isCorpse = true;
+            this.convertToCorpse();
+        }
+    }
+
+    updateStatAndPosition(statData)
+    {
+        if (this.hp !== statData.hp) {
+            this.hp = statData.hp;
+            if (this.hpText) {
+                this.hpText.setText(statData.hp + '/' + this.maxHp);
+            }
+        }
+
+        if (this.hp <= 0 && !this.isCorpse) {
+            this.isCorpse = true;
+            this.convertToCorpse();
+        }
+
+        this.updatePosition(statData);
+    }
+
+    convertToCorpse()
+    {
+        if (this.hpText) {
+            this.hpText.destroy();
+            this.setTint(0x333333);
+            // avoid late changes of damage effect
+            this.scene.time.delayedCall(110, () => this.setTint(0x333333), [], this);
+            this.setDepth(DEPTH_DEAD);
+            // TODO: Fix collisions with corpse!
+            this.scene.physics.add.world.disableBody(this);
+        }
+    }
+
+    updatePosition(posData)
+    {
+        if (this.isCorpse) {
+            return;
+        }
+
+        this.x = posData.x;
+        this.y = posData.y;
+
+        if (posData.isAttacking && !this.isAttacking) {
+            if (this.kind === 'demon') {
+                this.anims.play('demon_attack', true);
+            } else {
+                // example of attack effect
+                this.setTint(0x00ff00);
+            }
+
+            this.isAttacking = true;
+        } else if (!posData.isAttacking && this.isAttacking) {
+            if (this.kind === 'demon') {
+                this.anims.play('demon', true);
+            } else {
+                this.clearTint()
+            }
+            this.isAttacking = false;
+        }
+    }
+
+    static SpawnNewMonster(scene, statData)
+    {
+        let monster;
+        if (statData.kind === 'archer') {
+            monster = new Archer(scene, statData);
+        } else if (statData.kind === 'skeleton') {
+            monster = new Skeleton(scene, statData);
+        } else if (statData.kind === 'demon') {
+            monster = new Demon(scene, statData);
+        }  else {
+            console.error('Unknown monster kind:', statData.kind);
+            return null;
+        }
+
+        scene.add.existing(monster);
+        scene.physics.add.existing(monster);
+
+        return monster;
+    }
+}
+
+class Archer extends Monster
+{
+    kind = 'archer';
+    bowSprite;
+
+    constructor (scene, statData)
+    {
+        super(scene, statData, 'archer', 0);
+    }
+
+    preUpdate(time, delta)
+    {
+        super.preUpdate(time, delta);
+
+        if (this.bowSprite) {
+            this.bowSprite.x = this.x;
+            this.bowSprite.y = this.y;
+        }
+    }
+
+    spawn(statData)
+    {
+        super.spawn(statData);
+
+        this.bowSprite = this.scene.add.sprite(statData.x, statData.y, 'bow', 0)
+            .setScale(2)
+            .setDepth(DEPTH_MONSTER + 0.1) // above body
+            .setOrigin(0.5, 0.5);
+    }
+}
+
+class Skeleton extends Monster
+{
+    kind = 'skeleton';
+
+    constructor (scene, statData)
+    {
+        super(scene, statData, 'skeleton', 0);
+    }
+}
+
+class Demon extends Monster
+{
+    kind = 'demon';
+
+    constructor (scene, statData)
+    {
+        super(scene, statData, 'demon', 0);
+        this.anims.play('demon', true);
+    }
+}
