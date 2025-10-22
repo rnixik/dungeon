ffe468eedb9bbe8a247dade91fabf7a04f20d1ab
Roman Nix <rnix@yandex.ru>
2025-10-22 18:53:44 +0000

    Fix multi touch

--- public/js/scenes/scene.game.js
+++ public/js/scenes/scene.game.js
@@ -188,6 +188,7 @@ class Game extends Phaser.Scene {
         }
 
         // UI
+        this.input.addPointer(1); // allow 2 simultaneous pointers for mobile
         this.addMobileButtons();
     }
 
